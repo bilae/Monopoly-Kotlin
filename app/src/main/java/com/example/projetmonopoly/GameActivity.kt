cfe111b3b40faf49b7778a048d99c216a08aede0
NotPloZx <bilalelaisati@gmail.com>
2025-03-18 21:22:21 +0100

    Menu principal

---
+++ app/src/main/java/com/example/projetmonopoly/GameActivity.kt
@@ -0,0 +1,4 @@
+package com.example.projetmonopoly
+
+class GameActivity {
+}
--- app/src/main/java/com/example/projetmonopoly/MainActivity.kt
+++ app/src/main/java/com/example/projetmonopoly/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.projetmonopoly
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val spinnerBots: Spinner = findViewById(R.id.spinnerBots)
+        val btnStartGame: Button = findViewById(R.id.btnStartGame)
+
+        // Ici on choisit le nb de joueurs (des bots enft)
+        val botOptions = arrayOf("1 Bot", "2 Bots", "3 Bots", "4 Bots")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, botOptions)
+        spinnerBots.adapter = adapter
+
+        btnStartGame.setOnClickListener {
+            val selectedBots = spinnerBots.selectedItem.toString().split(" ")[0].toInt()
+
+            // Transfer de l'info vers le Game Activity
+            val intent = Intent(this, GameActivity::class.java)
+            intent.putExtra("NUM_BOTS", selectedBots)
+            startActivity(intent)
         }
     }
 }
