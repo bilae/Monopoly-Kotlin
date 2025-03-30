b3e6a34b26e99273c0fadee30537e53fce52808a
Goathier06 <gauthier.godeau@ulb.be>
2025-03-30 15:41:14 +0200

    nouvelle version plateau + pions

--- app/src/main/java/com/example/projetmonopoly/GameActivity.kt
+++ app/src/main/java/com/example/projetmonopoly/GameActivity.kt
@@ -2,6 +2,7 @@ package com.example.projetmonopoly
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
 
 class GameActivity : AppCompatActivity() {
 
@@ -17,9 +18,24 @@ class GameActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.bot3)
         )
 
+        val Plateau = findViewById<ImageView>(R.id.boardImage)
+
+        val pionViews = listOf(
+            findViewById<ImageView>(R.id.pion_vert),
+            findViewById<ImageView>(R.id.pion_jaune),
+            findViewById<ImageView>(R.id.pion_rouge),
+            findViewById<ImageView>(R.id.pion_bleu)
+        )
+
+
         // Afficher uniquement le nombre de bots sélectionné
-        for (i in 0 until 4) {
+        for (i in botViews.indices) {
             botViews[i].visibility = if (i < numBots) TextView.VISIBLE else TextView.INVISIBLE
         }
+
+        for (i in pionViews.indices) {
+            pionViews[i].visibility = if (i < numBots + 1) ImageView.VISIBLE else ImageView.INVISIBLE
+        }
+
     }
 }
